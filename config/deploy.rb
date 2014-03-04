78b6e598d8142869d445a6de21d22e0b2ce19e51
musik <cgicn@qq.com>
2014-03-04 17:43:15 +0800

    ignore

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "lccp"
 set :repository,  "git@github.com:musik/lccp.git"
 set :scm, :git
-set :deploy_to, "/home/muzik/www/955871"
+set :deploy_to, "/home/muzik/955871"
 ssh_options[:forward_agent] = true
 set :branch, "master"
 set :deploy_via, :remote_cache
@@ -16,13 +16,8 @@ role :db,  "lrt", :primary => true # This is where Rails migrations will run
 
 after "deploy:restart", "deploy:cleanup"
 set :shared_children, [
-          #"data/compile",
-          #"data/log",
-          #"data/dbbak",
-          "data/static",
-          "uploads/article",
-          "uploads/static",
-          "uploads/website"]
+          "file",
+    ]
 set :group_writable,true
 set :use_sudo, false
 
@@ -30,27 +25,22 @@ set :user, "muzik"
 set :group, "www-data"
 after "deploy:setup","app:setup"
 after "deploy:finalize_update","app:symlink"
-after "deploy:finalize_update","app:robot"
 
 namespace :app do
+  task :install do
+    upload "install/index.php.example","#{current_path}/install/index.php"
+  end
   task :setup do
-    upload './config.php', "#{shared_path}/"
-    upload './data/static', "#{shared_path}/", :via=> :scp, :recursive => true
-    run "chmod 775 #{shared_path}/config.php"
   end
   task :symlink do
-    tmp = user
-    set :user,'root'
-    run "chown -R #{tmp}:#{group} #{latest_release}"
-    if previous_release
-      ["data/install.lock"].each do |file|
-        run "if [ -f '#{previous_release}/#{file}' ]; then cp #{previous_release}/#{file} #{latest_release}/#{file}; fi;"
-      end
-    end
-    run "ln -s #{shared_path}/config.php #{latest_release}/config.php" 
-    run "touch #{shared_path}/robot.log"
-    run "chown #{tmp}:#{group} #{shared_path}/robot.log"
-    run "ln -s #{shared_path}/robot.log #{latest_release}/robot.log"
+    #tmp = user
+    #set :user,'root'
+    #run "chown -R #{tmp}:#{group} #{latest_release}"
+    #if previous_release
+      #["data/install.lock"].each do |file|
+        #run "if [ -f '#{previous_release}/#{file}' ]; then cp #{previous_release}/#{file} #{latest_release}/#{file}; fi;"
+      #end
+    #end
   end
   task :remove do
     ["config.php","data/install.lock"].each do |file|
@@ -62,8 +52,5 @@ namespace :app do
     set :user,'root'
     upload './config/nginx.dir.conf','/etc/nginx/global/dir.conf'
     run "service nginx reload"
-  end
-  task :robot do
-    run "chmod +x #{current_path}/robot"
   end
 end
